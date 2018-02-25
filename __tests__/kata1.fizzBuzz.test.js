082682093f87d203c6ca9a79ef4679e5cfbeb5ff
Rob Lihou <rob@visiosound.co.uk>
2018-02-25 22:18:18 +0000

    completed the fizzbuzz task

--- __tests__/kata1.fizzBuzz.test.js
+++ __tests__/kata1.fizzBuzz.test.js
@@ -2,18 +2,18 @@ import { fizzBuzz } from '../src'
 
 describe('fizzBuzz', () => {
   it('returns Fizz when passed a multiple of 3', () => {
-
+    expect(fizzBuzz(9)).toEqual('Fizz');
   })
 
   it('returns Buzz when passed a multiple of 5', () => {
-
+    expect(fizzBuzz(25)).toEqual('Buzz');  
   })
 
   it('returns FizzBuzz when passed a multiple 3 and 5', () => {
-
+    expect(fizzBuzz(15)).toEqual('FizzBuzz');
   })
 
   it('returns the number when it isn\'t a multiple of 3 or 5', () => {
-
+    expect(fizzBuzz(16)).toEqual('16');    
   })
 })
--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,5 +1,15 @@
 const fizzBuzz = (number) => {
-
+    let str = '';
+    if(number % 3 === 0){
+        str += 'Fizz';
+    }
+    if(number % 5 === 0){
+        str += 'Buzz';
+    }
+    if(str === ''){
+        str += number;
+    }
+    return str;
 }
 
 export default fizzBuzz
